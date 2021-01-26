8b347558c7db8dd241f98024a61556219dbfa040
avison <avisonlivingstone@gmail.com>
2021-01-26 07:52:33 +0100

    First Capstone Project notebook

---
+++ Capstone_1.py
@@ -0,0 +1,6 @@
+# This notebook is to carry out the Captsone projects (Battle of Neigbourhoods) from the IBM Data Science Specialist course, via Coursera!!!
+
+import pandas as pd
+import numpy as np
+
+print('Hello Capstone Project Course!')
