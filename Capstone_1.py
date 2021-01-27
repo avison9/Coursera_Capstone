05ec46e2ac16ad774b1f3cec471b13ad63c34fb4
avison <avisonlivingstone@gmail.com>
2021-01-27 16:02:26 +0100

    First Capstone Task

--- Capstone_1.py
+++ Capstone_1.py
@@ -4,20 +4,3 @@ import pandas as pd
 import numpy as np
 
 print('Hello Capstone Project Course!')
-a =13
-b = 11
-c = 12
-d = 15
-e = 16
-f = 99
-g = 24
-h = 3
-i = 9
-lis = {
-	'Jaccard':[a,b,c,d],
-	'fi score':['16','33','NA','77'],
-	'plu':['11','244','54','NA']
-}
-index = ['knn','DT','SVM','Poly']
-df =pd.DataFrame(lis, index= index)
-print(df)
