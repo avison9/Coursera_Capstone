5e833597cc055c04e07c35bbe4cb2ef5fd978ea7
avison <avisonlivingstone@gmail.com>
2021-01-29 09:59:27 +0100

    Segmenting Task

--- Capstone_1.py
+++
@@ -1,6 +0,0 @@
-# This notebook is to carry out the Captsone projects (Battle of Neigbourhoods) from the IBM Data Science Specialist course, via Coursera!!!
-
-import pandas as pd
-import numpy as np
-
-print('Hello Capstone Project Course!')
